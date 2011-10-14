dfbd48ffb5436dabbb70c6f7705316f1c32c4e1a
Jason Torres <jason.e.torres@gmail.com>
2011-10-14 17:04:15 +0800

    Hardcore the samples url for the moment

--- app/models/heypal.rb
+++ app/models/heypal.rb
@@ -1,6 +1,7 @@
 module Heypal
   
-  BASE_URL = Rails.env.development? ? "http://localhost:3005" : "http://frontend-heypal.heroku.com"
+  #BASE_URL = Rails.env.development? ? "http://localhost:3005" : "http://frontend-heypal.heroku.com"
+  BASE_URL = "http://frontend-heypal.heroku.com"
 
   PRODUCTS_URL = BASE_URL + "/samples/products.json"
   PRODUCT_URL = BASE_URL + "/samples/product.json"

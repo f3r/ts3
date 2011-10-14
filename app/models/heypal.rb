eaecd433b41d8d505e1c885ebc61f538ada56274
Jason Torres <jason.e.torres@gmail.com>
2011-10-14 16:54:18 +0800

    Eh. update to actual http and update Gemfile.lock again

--- app/models/heypal.rb
+++ app/models/heypal.rb
@@ -1,6 +1,6 @@
 module Heypal
   
-  BASE_URL = Rails.env.development? ? "http://localhost:3005" : "frontend-heypal.heroku.com"
+  BASE_URL = Rails.env.development? ? "http://localhost:3005" : "http://frontend-heypal.heroku.com"
 
   PRODUCTS_URL = BASE_URL + "/samples/products.json"
   PRODUCT_URL = BASE_URL + "/samples/product.json"

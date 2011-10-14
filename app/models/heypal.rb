cf8d3cada19cb5667efea1a6353ccb714e736178
Jason Torres <jason.e.torres@gmail.com>
2011-10-14 17:13:36 +0800

    Try a local http url for JSON samples

--- app/models/heypal.rb
+++ app/models/heypal.rb
@@ -1,7 +1,7 @@
 module Heypal
   
-  #BASE_URL = Rails.env.development? ? "http://localhost:3005" : "http://frontend-heypal.heroku.com"
-  BASE_URL = "http://frontend-heypal.heroku.com"
+  BASE_URL = Rails.env.development? ? "http://localhost:3005" : "http://127.0.0.1"
+  #BASE_URL = "http://frontend-heypal.heroku.com"
 
   PRODUCTS_URL = BASE_URL + "/samples/products.json"
   PRODUCT_URL = BASE_URL + "/samples/product.json"

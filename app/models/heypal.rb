7d0de7192883cbe757898f5e4043e0574a5b31ab
Jason Torres <jason.e.torres@gmail.com>
2011-10-14 17:19:04 +0800

    Host the sample files somewhere else

--- app/models/heypal.rb
+++ app/models/heypal.rb
@@ -1,7 +1,6 @@
 module Heypal
   
-  BASE_URL = Rails.env.development? ? "http://localhost:3005" : "http://127.0.0.1"
-  #BASE_URL = "http://frontend-heypal.heroku.com"
+  BASE_URL = Rails.env.development? ? "http://localhost:3005" : "http://morecoffeeplease.s3.amazonaws.com"
 
   PRODUCTS_URL = BASE_URL + "/samples/products.json"
   PRODUCT_URL = BASE_URL + "/samples/product.json"

314a0c0273f25012ec120d135ae2d9cf2970c915
Jason Torres <jason.e.torres@gmail.com>
2011-10-26 13:20:31 +0800

    Create sessions#connect for connecting social networks

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -26,33 +26,42 @@ class SessionsController < ApplicationController
     
     if omniauth['provider'] == 'twitter'
  
-
       # CHECK if the oauth is valid     
-      Heypal::Session.valid_oauth?({:oauth_token => omniauth['credentials']['token']})
-
-      # CREATE the Record?
-      Heypal::Session.create_oauth({:access_token => omniauth['credentials']['secret'], :oauth_token => omniauth['credentials']['token']})
-
-      # 
-      @heypal_session = Heypal::Session.signin_via_twitter({:access_token => omniauth['credentials']['secret'], :oauth_token => omniauth['credentials']['token']})
-        
+      if Heypal::Session.valid_oauth?({:oauth_token => omniauth['credentials']['token']})
+  
+        # Sign In 
+        @heypal_session = Heypal::Session.signin_via_twitter({:access_token => omniauth['credentials']['secret'], :oauth_token => omniauth['credentials']['token']})
+      
+      else
+        # Q 1: Do I sign up the user w/o the password OR shall I ask for the password
+      end 
 
     elsif omniauth['provider'] == 'facebook'
       
-      @heypal_session = Heypal::Session.create_oauth({:oauth_token => omniauth['credentials']['token']}) 
-      @heypal_session = Heypal::Session.signin_via_facebook({:oauth_token => omniauth['credentials']['token']})
+      #@heypal_session = Heypal::Session.create_oauth({:oauth_token => omniauth['credentials']['token']}) 
+      #@heypal_session = Heypal::Session.signin_via_facebook({:oauth_token => omniauth['credentials']['token']})
 
     end
 
-    if @heypal_session.valid?
-      redirect_to '/'
-    else
+    #if @heypal_session.valid?
+      #redirect_to '/'
+    #else
 
-    end
+    #end
 
-      #render :text => {:provider => omniauth['provider'], :uid => omniauth['uid'], :token => omniauth['credentials']['token'], :secret => omniauth['credentials']['secret']}      
+    render :text => {:provider => omniauth['provider'], :uid => omniauth['uid'], :token => omniauth['credentials']['token'], :secret => omniauth['credentials']['secret']}      
 
+  end
 
+  ##
+  # Attach a facebook/email account to an existing account
+  #
+  def connect
+    # Create the Record?
+    # This should be sending the email as well OR Facebook/Twitter UID, specially for Facebook because it doesn't have access token
+    Heypal::Session.create_oauth({:access_token => omniauth['credentials']['secret'], :oauth_token => omniauth['credentials']['token']})    
+
+    redirect_to '/'
   end
 
   def destroy

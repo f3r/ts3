89fafc7331fed3da1b1594301f7163e4da095251
Manolet Balanay <manolet@proudcloud.net>
2011-10-27 17:43:55 +0800

    Form for User Edit Page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,11 @@
 module UsersHelper
   def full_name(user)
-    name = [user['first_name'], user['last_name']].join(',')
-    name unless name.eql?(',')
+    name = [user['first_name'], user['last_name']].join(' ')
+    name unless name.eql?(' ')
+  end
+  
+  def nickname(user)
+    nick = "#{user['first_name'][0]}#{user['last_name']}"
+    nick unless nick.blank?
   end
 end

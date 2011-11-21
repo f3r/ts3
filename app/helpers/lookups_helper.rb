0824af556ccb38886df2909fd3e352199af42316
index <aeguintu@gmail.com>
2011-11-21 17:21:52 +0800

    Fixed the currency and language.

--- app/helpers/lookups_helper.rb
+++ app/helpers/lookups_helper.rb
@@ -27,12 +27,12 @@ module LookupsHelper
   end
 
   def pref_language_list
-    current = (self.get_language || 'en')
+    current = get_current_language
     select_tag :pref_language, options_for_select(LANGUAGES.map{ |l, t| [t, l]}, current), :class => "hide preference-entry", "data-current" => current
   end
 
   def pref_currency_list
-    current = (self.get_currency || 'USD')
+    current = self.get_current_currency
     select_tag :pref_currency, options_for_select(CURRENCIES.map{ |c, t| [t, c]}, current), :class => "hide preference-entry", "data-current" => current
   end
 
@@ -44,8 +44,12 @@ module LookupsHelper
     CURRENCIES[self.get_currency] || 'US$'
   end
 
-  def current_currency
-    get_currency
+  def get_current_language
+    get_language || 'en'
+  end
+
+  def get_current_currency
+    get_currency || 'USD'
   end
 
   # Should be of this format:
@@ -63,10 +67,10 @@ module LookupsHelper
 
   protected
   def get_language
-    (current_user['pref_language'] if logged_in?) || cookies[:pref_language] || nil
+    (current_user['pref_language'] if logged_in? && !current_user['pref_language'].blank?) || cookies[:pref_language] || nil
   end
 
   def get_currency
-    (current_user['pref_currency'] if logged_in?) || cookies[:pref_currency] || nil
+    (current_user['pref_currency'] if logged_in? && !current_user['pref_currency'].blank?) || cookies[:pref_currency] || nil
   end
 end

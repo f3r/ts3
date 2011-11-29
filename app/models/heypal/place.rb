061b731961fca275758ccbe0c51f608d61f8bc01
Jason Torres <jason.e.torres@gmail.com>
2011-11-30 07:26:23 +0800

    Change the attributes array to a cleaner syntax Added the form tag for search

--- app/models/heypal/place.rb
+++ app/models/heypal/place.rb
@@ -3,23 +3,14 @@ class Heypal::Place < Heypal::Base
 
   set_resource_path '/places.json'
 
-  @@general_attributes = %w(
-    title description place_type_id city_id num_bedrooms num_beds num_bathrooms size_sqm size_sqf max_guests size_unit access_token published 
-  )
+  @@general_attributes = ["title", "description", "place_type_id", "city_id", "num_bedrooms", "num_beds", "num_bathrooms", "size_sqm", "size_sqf", "max_guests", "size_unit", "access_token", "published"]
+  @@geo_attributes = ["address_1", "address_2", "zip", "directions", "state_id", "country_id"]
 
-  @@geo_attributes = %w(
-    address_1 address_2 zip directions state_id country_id 
-  )
+  @@amenities_attributes = ["amenities_aircon", "amenities_breakfast", "amenities_buzzer_intercom", "amenities_cable_tv", "amenities_dryer", "amenities_doorman", "amenities_elevator", "amenities_family_friendly", "amenities_gym", "amenities_hot_tub", "amenities_kitchen", "amenities_handicap", "amenities_heating", "amenities_hot_water", "amenities_internet", "amenities_internet_wifi", "amenities_jacuzzi", "amenities_parking_included", "amenities_pets_allowed", "amenities_pool", "amenities_smoking_allowed", "amenities_suitable_events", "amenities_tennis", "amenities_tv", "amenities_washer"] 
 
-  @@amenities_attributes = %w(amenities_aircon amenities_breakfast amenities_buzzer_intercom amenities_cable_tv amenities_dryer amenities_doorman amenities_elevator amenities_family_friendly amenities_gym amenities_hot_tub amenities_kitchen amenities_handicap amenities_heating amenities_hot_water amenities_internet amenities_internet_wifi amenities_jacuzzi amenities_parking_included amenities_pets_allowed amenities_pool amenities_smoking_allowed amenities_suitable_events amenities_tennis amenities_tv amenities_washer)
+  @@pricing_attributes = ["currency", "price_per_night", "price_per_week", "price_per_month", "price_final_cleanup", "price_security_deposit"] 
 
-  @@pricing_attributes = %w(
-    currency price_per_night price_per_week price_per_month price_final_cleanup price_security_deposit 
-
-  )
-  @@terms_attributes = %w(
-    check_in_after check_out_before minimum_stay_days maximum_stay_days house_rules cancellation_policy
-  )
+  @@terms_attributes = ["check_in_after", "check_out_before", "minimum_stay_days", "maximum_stay_days", "house_rules", "cancellation_policy"] 
 
   @@attributes = @@general_attributes + @@geo_attributes + @@amenities_attributes + @@pricing_attributes + @@terms_attributes
 
@@ -96,7 +87,7 @@ class Heypal::Place < Heypal::Base
       p    
     end
 
-    def search(oarams = {})
+    def search(oarams =  {})
 
     end
 

1f9f91273859ac54e6a50fe8d0bd3a4057fd1710
Jorge Yau <jorgeyau@gmail.com>
2011-12-07 15:03:12 -0500

    small fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,18 @@ class ApplicationController < ActionController::Base
   include AuthenticationHelper
   include LookupsHelper
   include AvailabilitiesHelper
+  before_filter :instantiate_controller_and_action_names
 
   def params_with_token(resource)
     p = params[resource]
     p[:access_token] = current_token
     p 
   end
+
+  private
+  def instantiate_controller_and_action_names
+    @current_action = action_name
+    @current_controller = controller_name
+  end
+
 end

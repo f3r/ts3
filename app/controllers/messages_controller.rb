88b33dd99d850235f967d0904e61685b9c8a0082
Alec Guintu <aeguintu@gmail.com>
2011-12-09 10:20:46 +0800

    Changed how to get user your conversing with.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,16 +9,8 @@ class MessagesController < ApplicationController
 
   # Retrieves all messages with a user
   def conversation
+    @with = Heypal::User.info("id" => params[:id])
     @messages  = Heypal::Message.messages({"id" => params[:id],"access_token" => current_token})
-    if !@messages.nil? && !@messages['messages'].blank?
-      if @messages['messages'][0]['from']['id'] == current_user['id']
-        @with_name = "#{@messages['messages'][0]['to']['first_name'][0]}. #{@messages['messages'][0]['to']['last_name']}"
-        @with_id   = @messages['messages'][0]['to']['id']
-      else
-        @with_name = "#{@messages['messages'][0]['from']['first_name'][0]}. #{@messages['messages'][0]['from']['last_name']}"
-        @with_id   = @messages['messages'][0]['from']['id']
-      end
-    end
   end
 
   # Posts a new message to a user

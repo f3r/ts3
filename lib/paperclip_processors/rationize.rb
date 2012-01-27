93d92ae4f2e48afa69679fe3fcf88abada37f76e
Alec Guintu <aeguintu@gmail.com>
2012-01-27 16:10:21 +0800

    [FIXES #22182635] Cropping done to make image of 3:2 first then resized and watermarked.

--- lib/paperclip_processors/rationize.rb
+++ lib/paperclip_processors/rationize.rb
@@ -2,13 +2,14 @@ module Paperclip
   class Rationize < Processor
     # Plan is to crop to 3:2 ratio first then watermark processor would resize and watermark
     attr_accessor :current_geometry, :target_geometry, :format
-    
+
     def initialize(file, options = {}, attachment = nil)
       super
       geometry            = options[:geometry]
       @file               = file
       @target_geometry    = Geometry.parse(geometry)
       @current_geometry   = Geometry.from_file(@file)
+      @whiny              = options[:whiny].nil? ? true : options[:whiny]
 
       @format             = options[:format]
       @current_format     = File.extname(@file.path)
@@ -19,24 +20,32 @@ module Paperclip
       dst = Tempfile.new([@basename, @format].compact.join("."))
       dst.binmode
 
-      puts @current_geometry.height
-      puts @current_geometry.width
-
       if @current_geometry.vertical?
-        new_height = @current_geometry.height
-        new_width = (@current_geometry.width / @current_geometry.height) * new_height
-        puts "New width : #{new_width.inspect}"
-        puts "New height : #{new_height.inspect}"
-      elsif @current_geometry.horizontal?
         new_width = @current_geometry.width
-        new_height = (@current_geometry.height / @current_geometry.width) * new_width
-
-        puts "New width : #{new_width.inspect}"
-        puts "New height : #{new_height.inspect}"
-
+        new_height = new_width * 3 / 2
+      elsif @current_geometry.horizontal?
+        new_height = @current_geometry.height
+        new_width = new_height * 3 / 2
       end
 
-      @file
+      params = "#{fromfile} -gravity Center -crop #{new_width.to_i}x#{new_height.to_i}+0+0 +repage #{tofile(dst)}"
+
+      begin
+        success = Paperclip.run("convert", params)
+      rescue PaperclipCommandLineError
+        raise PaperclipError, "There was an error processing the watermark for #{@basename}" if @whiny
+      end
+
+      dst
     end
+
+    def fromfile
+      "\"#{ File.expand_path(@file.path) }[0]\""
+    end
+
+    def tofile(destination)
+      "\"#{ File.expand_path(destination.path) }[0]\""
+    end
+
   end
 end

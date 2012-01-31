c95bce1c4aef2947dc7ac4496221138cddfe8422
Nicolas Gaivironsky <ngaivi@gmail.com>
2012-01-31 14:10:28 +0800

    Redirect to page after login

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,12 @@
 module HomeHelper
+  def login_path_with_ref
+    black_list = ['/', '/signup', '/login']
+    current_path = request.path
+
+    if current_path && !black_list.include?(current_path)
+      login_path(:ref => request.path)
+    else
+      login_path
+    end
+  end
 end

36463ed89e62d4c493165f16e3344e1994f99f46
Nicolas Gaivironsky <ngaivi@gmail.com>
2012-02-02 16:57:49 +0800

    Photography FAQ page

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -118,8 +118,6 @@ $(function() {
     $(this).toggleClass('sticky', direction === "down");
     event.stopPropagation();
 	}, {offset: 52});  // NOTE: when you change this, goto heypal.less -> .sticky and change top attr
-
-
 });
 
 function add_datepicker() {
@@ -213,4 +211,26 @@ function disabled_question_button() {
   }).keyup();
 }
 
-
+var Expandable = {
+  initialize: function(selector){
+	var container = $(selector);
+	container.find('.expandable').each(function(){
+	  var section = $(this);
+	  section.find('.inner').hide();
+	  section.addClass('collapsed');
+	  section.click(function(){
+		if(section.hasClass('collapsed')){
+		  section.find('a').hide();
+		  section.find('.inner').slideDown('slow');
+		  section.removeClass('collapsed');
+		  return false;
+		} else {
+		  section.find('a').show();
+		  section.find('.inner').slideUp('slow', function(){
+		    section.addClass('collapsed');	
+		  });
+		}
+	  })
+	})
+  }
+}

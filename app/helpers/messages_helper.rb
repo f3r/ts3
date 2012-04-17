5531b80ca5b42ea4ee1af92e10b919a39f5e0110
Nicolas Gaivironsky <ngaivi@gmail.com>
2012-04-17 22:48:33 +0800

    Adding system messages

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -5,4 +5,16 @@ module MessagesHelper
     initials <<  "#{from['last_name'][0]}." if from['last_name']
     initials.join(' ')
   end
+
+  def system_message_body(system_msg_id)
+    t("workflow_system_msg_#{system_msg_id}".to_sym)
+  end
+
+  def conversation_title
+    if @inquiry
+      "#{t(:inquiry_on)} #{@inquiry['place_title']}"
+    else
+      "#{t(:conversation_with)} #{@conversation['from']['first_name']}"
+    end
+  end
 end
--- config/initializers/active_merchant.rb
+++ config/initializers/active_merchant.rb
@@ -1,10 +1,4 @@
-if Rails.env.production?
-  PAYPAL_ACCOUNT = 'sales@heypal.com'
-else
-  PAYPAL_ACCOUNT = 'nico_1334303615_biz@heypal.com'
-  ActiveMerchant::Billing::Base.mode = :test
-end
-
+ActiveMerchant::Billing::Base.mode = PAYPAL_MODE.to_sym
 require 'active_merchant/billing/integrations/action_view_helper'
 
 ActionView::Base.send(:include, ActiveMerchant::Billing::Integrations::ActionViewHelper)

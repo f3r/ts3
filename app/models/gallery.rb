ef6528b13f951993efda85b4e6895641c8926780
Vinu Varghese <vinu@pollenizer.in>
2012-05-15 15:24:23 +0530

    [FIXES #29371441] - dependent destroy

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,5 +1,5 @@
 class Gallery < ActiveRecord::Base
   validates_presence_of [:name], :message => "Name can't be empty"
   validates_format_of :name, :with => /\A(^[a-z]{1}[a-z0-9_]+)\Z/, :message => "Invalid name"
-  has_many :gallery_items
+  has_many :gallery_items, :dependent => :destroy
 end

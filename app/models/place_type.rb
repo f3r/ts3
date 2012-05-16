2d833c1a3e1a5c14fdd9a0fbd8646a110ecc2687
Vinu Varghese <vinu@pollenizer.in>
2012-05-16 10:40:14 +0530

    [FIXES #29535747] - fixes for error message

--- app/models/place_type.rb
+++ app/models/place_type.rb
@@ -1,7 +1,7 @@
 class PlaceType < ActiveRecord::Base
   using_access_control
-  validates_uniqueness_of :name, :message => "100"
-  validates_presence_of :name, :message => "101"
+  validates_uniqueness_of :name, :message => "Name should be unique"
+  validates_presence_of :name, :message => "Name should not be empty"
   attr_accessible :name
   has_many :places
   after_commit  :delete_cache

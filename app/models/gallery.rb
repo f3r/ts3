441e5a1faaa8d594e8a4a8f5fca8ecc762d1356b
Vinu Varghese <vinu@pollenizer.in>
2012-05-16 14:09:52 +0530

    Name should be unique

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,5 +1,6 @@
 class Gallery < ActiveRecord::Base
   validates_presence_of [:name], :message => "Name can't be empty"
   validates_format_of :name, :with => /\A(^[a-z]{1}[a-z0-9_]+)\Z/, :message => "Invalid name"
+  validates_uniqueness_of :name, :message => "Name should be unique"
   has_many :gallery_items, :dependent => :destroy
 end

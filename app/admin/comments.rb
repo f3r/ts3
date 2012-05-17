916477f2040678ee55329ddc1a16f345e87a6304
Nicolas Gaivironsky <ngaivi@gmail.com>
2012-05-17 16:12:30 +0800

    Showing answers on questions active admin

--- app/admin/comments.rb
+++ app/admin/comments.rb
@@ -2,25 +2,25 @@ ActiveAdmin.register Comment, :as => "Question" do
   menu :priority => 5
   #We don't need new question button
   actions :all, :except => :new
-  
-  scope :all, :default => true
-  scope :questions
-  
+
+  scope :questions, :default => true
+
   filter :comment
-  
+
   index do
     id_column
     column :place, :sortable => :place_id
-    column 'Question', :comment
     column :user, :sortable => :user_id
+    column 'Question', :comment
+    column('Answer') {|q| q.reply.comment if q.reply }
     column :created_at
-    column("Type") {|question| status_tag(question.owner ? 'ANSWER': 'QUESTION')} 
+    column(:responded_at) {|q| l(q.reply.created_at, :format => :long) if q.reply }
     default_actions
   end
-  
+
   show do |question|
     attributes_table do
-      row :id    
+      row :id
       row :place
       row("QUESTION") {question.comment}
       row :user
@@ -32,20 +32,20 @@ ActiveAdmin.register Comment, :as => "Question" do
             link_to 'View', admin_question_path(question.answers[0])
           end
         end
-      
+
       row :created_at
     end
   end
-  
+
   form do |f|
     f.inputs do
       f.input :id, :input_html => { :disabled => true }
-      f.input :place, :input_html => { :disabled => true } 
+      f.input :place, :input_html => { :disabled => true }
       f.input :comment, :label => "Question"
       f.input :user, :input_html => { :disabled => true }
-      f.input :created_at, :input_html => { :disabled => true } 
+      f.input :created_at, :input_html => { :disabled => true }
     end
     f.buttons
   end
-  
+
 end

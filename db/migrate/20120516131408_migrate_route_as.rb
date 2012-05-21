67c6d52a9d82678b7016f425861e86511f5dad66
rajeshs <rajesh.s@xminds.in>
2012-05-21 13:37:34 +0530

    [FIXES #29531085] Pages should have dynamic routes

--- db/migrate/20120516131408_migrate_route_as.rb
+++ db/migrate/20120516131408_migrate_route_as.rb
@@ -1,10 +1,10 @@
 class MigrateRouteAs < ActiveRecord::Migration
   def up
-     execute "UPDATE `cmspages` SET `route_as`= 'home_how' where id =1" 
-     execute "UPDATE `cmspages` SET `route_as`= 'home_why' where id =2" 
-     execute "UPDATE `cmspages` SET `route_as`= 'cityguide_sg' where id =3" 
-     execute "UPDATE `cmspages` SET `route_as`= 'cityguide_hk' where id =4" 
-     execute "UPDATE `cmspages` SET `route_as`= 'cityguide_kl' where id =5" 
+     execute "UPDATE `cmspages` SET `route_as`= 'home_how' where `page_url` ='how'" 
+     execute "UPDATE `cmspages` SET `route_as`= 'home_why' where `page_url` ='why'" 
+     execute "UPDATE `cmspages` SET `route_as`= 'cityguide_sg' where `page_url` ='sg'" 
+     execute "UPDATE `cmspages` SET `route_as`= 'cityguide_hk' where `page_url` ='hk'" 
+     execute "UPDATE `cmspages` SET `route_as`= 'cityguide_kl' where `page_url` ='kl'" 
   end
 
   def down

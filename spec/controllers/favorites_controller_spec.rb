905bbcf6e1e3d240754bc1a55bf6332fd7a96790
Nicolas Gaivironsky <ngaivi@gmail.com>
2012-05-31 16:12:12 +0800

    Handle favorites workflow on the front end

--- spec/controllers/favorites_controller_spec.rb
+++ spec/controllers/favorites_controller_spec.rb
@@ -2,32 +2,29 @@ require 'spec_helper'
 
 describe FavoritesController do
   before(:each) do
-    @user = FactoryGirl.create(:user)
+    @user = create(:user)
     login_as @user
-    # @user = double("user", :id => 32)
-    # session['current_user'] = @user
-    place = double(:id => 1)
-    place.stub(:[]).with('id').and_return(1)
-    Heypal::Place.stub(:find).and_return(place)
+
+    @place = create(:published_place)
   end
 
   it "creates a new favorite" do
-    lambda {
-      post :create, :place_id => 23
-    }.should change(Favorite, :count).by(1)
+    expect {
+      post :create, :place_id => @place.id
+    }.to change(Favorite, :count).by(1)
   end
 
   it "removes a favorite" do
-    fav = FactoryGirl.create(:favorite, :user_id => @user.id)
-    lambda {
-      delete :destroy, :id => fav.favorable_id, :place_id => fav.favorable_id
-    }.should change(Favorite, :count).by(-1)
+    fav = create(:favorite, :user_id => @user.id, :favorable_id => @place.id, :favorable_type => 'Place')
+    expect {
+      delete :destroy, :id => @place.id, :place_id => @place.id
+    }.to change(Favorite, :count).by(-1)
   end
 
   it "doesn't remove another user favorite" do
     fav = FactoryGirl.create(:favorite, :user_id => @user.id + 1)
-    lambda {
-      delete :destroy , :id => fav.id, :place_id => fav.favorable_id
-    }.should raise_error
+    expect {
+      delete :destroy , :id =>  @place.id, :place_id => @place.id
+    }.to raise_error
   end
 end

449eedde1ed8a648302767e32590bed60a8d345e
Vinu Varghese <vinu@pollenizer.in>
2012-06-05 18:13:00 +0530

    #[FIXES - 29683383] - data to funnel added

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -15,5 +15,12 @@ ActiveAdmin::Dashboards.build do
       render('/admin/chart', :title => 'Places', :stats => stats)
     end
   end
+  
+  section "Data Funnel", :priority => 40 do
+    div do
+      image_tag("http://chart.apis.google.com/chart?" + AdminDashboard.conversion_funnel_as_g_chart_params)
+    end
+  end
+  
 
 end

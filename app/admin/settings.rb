cd9fcb23c5624bb32a17e929fc4365cfa8b1a16a
Fer Martin <fer@heypal.com>
2012-06-12 14:58:48 +0800

    Improved descriptions

--- app/admin/settings.rb
+++ app/admin/settings.rb
@@ -16,7 +16,7 @@ ActiveAdmin.register SiteConfig, :as => 'Settings' do
   end
 
   form do |f|
-    f.inputs "Basic" do
+    f.inputs "Basic Settings" do
       f.input :site_name
       f.input :site_url
       f.input :site_tagline
@@ -24,17 +24,19 @@ ActiveAdmin.register SiteConfig, :as => 'Settings' do
       f.input :support_email
       f.input :mail_bcc
     end
-    f.inputs "Credentials" do
+    f.inputs "Credentials for external services" do
       f.input :gae_tracking_code
       f.input :fb_app_id
       f.input :fb_app_secret
       f.input :tw_app_id
       f.input :tw_app_secret
     end
-    f.inputs "Meta Content" do
-      f.input :custom_meta
-      f.input :meta_description
-      f.input :meta_keywords
+    f.inputs "SEO Enhancement" do
+      f.input :custom_meta,
+        :label => "Custom Meta Tags",
+        :hint => "Please add with the format: <META NAME= \"Your Meta Tag Key\" content=\"Your Meta Tag Content\"> -- Please visit http://en.wikipedia.org/wiki/Meta_element for more info"
+      f.input :meta_description, :hint => "Create a description of your site for search engines"
+      f.input :meta_keywords, :hint => "Add all keywords that describe your site. Separated by commas"
     end
     f.buttons
   end

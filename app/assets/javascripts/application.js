bd8f0be3b57462faa452d078796e3563b4f64b27
Jorge Yau <jorgeyau@gmail.com>
2012-06-12 10:57:58 -0500

    repacked bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,21 +23,7 @@
 //= require jquery.bookmark
 //= require modernizr
 //= require fullcalendar.min
-
-// TODO: Remove unused
-//= require bootstrap/bootstrap-transition.js
-//= require bootstrap/bootstrap-alert.js
-//= require bootstrap/bootstrap-button.js
-//= require bootstrap/bootstrap-carousel.js
-// require bootstrap/bootstrap-collapse.js
-//= require bootstrap/bootstrap-dropdown.js
-//= require bootstrap/bootstrap-modal.js
-//= require bootstrap/bootstrap-tooltip.js
-//= require bootstrap/bootstrap-popover.js
-// require bootstrap/bootstrap-scrollspy.js
-//= require bootstrap/bootstrap-tab.js
-//= require bootstrap/bootstrap-typeahead.js
-
+//= require twitter/bootstrap
 //= require_self
 //= require_tree .
 

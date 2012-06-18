366243084a9405fa247a4c0e5d54d14943ecea3e
Fer Martin <fer@heypal.com>
2012-06-18 14:50:35 +0800

    Remove user/create from admin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,11 +1,11 @@
 ActiveAdmin.register User do
   menu :priority => 1
-  actions :all, :except => [:destroy]
-  
+  actions :all, :except => [:create, :destroy]
+
   controller do
     helper 'admin/users'
   end
-  
+
   scope :all, :default => true
   scope :consumer
   scope :agent
@@ -15,35 +15,35 @@ ActiveAdmin.register User do
   filter :first_name
   filter :last_name
   filter :created_at
-  
+
   index do |place|
     id_column
     column :email
     column :full_name
-    column(:role)         {|user| status_tag(user.role) } 
+    column(:role)         {|user| status_tag(user.role) }
     column :created_at
     column :confirmed_at
     column :last_sign_in_at
     column("Actions")     {|user| user_links_column(user) }
   end
-  
+
   show do |ad|
     rows = default_attribute_table_rows.reject {|a| a =~ /password/}
     attributes_table *rows do
     end
   end
-  
-  
+
+
   # Make Agent
   action_item :only => :show do
-    link_to('Make Agent', make_agent_admin_user_path(user), :method => :put, 
+    link_to('Make Agent', make_agent_admin_user_path(user), :method => :put,
       :confirm => 'Are you sure you want to turn the user into an agent?') if user.consumer?
   end
-  
+
   member_action :make_agent, :method => :put do
     user = User.find(params[:id])
     user.update_attribute(:role, 'agent')
     redirect_to({:action => :show}, :notice => "The user is now an agent")
   end
-  
+
 end

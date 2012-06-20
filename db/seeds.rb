9291b7204162c472be06648fac0954725d1477a8
rajeshs <rajesh.s@xminds.in>
2012-06-20 14:31:07 +0530

    [FIXES #31334915] admin SBAT manage favicon

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,5 @@
 City.create(name: "Singapore", lat: 1.28967, lon: 103.85, country: "Singapore", country_code: "SG", active: true )
 Currency.create(name: "Us Dollar", symbol: "$", country: "USA", currency_code: "USD", active: 1, position: 1,currency_abbreviation:"US" )
+siteConfig = SiteConfig.find_or_create_by_site_name(:site_name => 'SquareStayz')
+siteConfig.fav_icon = File.open("#{Rails.root}/db/rake_seed_images/favicon.ico")
+siteConfig.save!

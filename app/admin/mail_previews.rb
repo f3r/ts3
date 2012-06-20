23050be98822833d8d8bfac057908cf515150944
Jorge Yau <jorgeyau@gmail.com>
2012-06-20 15:50:41 -0500

    [FIXES #31449221] minor fix on labels

--- app/admin/mail_previews.rb
+++ app/admin/mail_previews.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register_page "Mail Previews" do
   menu :parent => 'CMS'
 
   content do
-    panel "Templates" do
+    panel "Template Preview" do
         render 'index', :layout => 'active_admin'
     end
   end  

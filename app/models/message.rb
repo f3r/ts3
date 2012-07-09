7cde44a6f77af3018d8be4695f282f5d01af911a
Nicolas Gaivironsky <ngaivi@gmail.com>
2012-07-09 10:52:57 +0800

    Show system messages on the inbox list

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,4 +6,16 @@ class Message < ActiveRecord::Base
     msg_id = read_attribute(:system_msg_id)
     msg_id.to_sym if msg_id
   end
+
+  def summary
+    if self.system?
+      "[#{self.system_message_body}]"
+    else
+      self.body
+    end
+  end
+
+  def system_message_body
+    I18n.t("workflow.system_msg_#{self.system_msg_id}".to_sym)
+  end
 end

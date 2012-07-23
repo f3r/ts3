fe12f8154ab22f597ef0d2c77cfee185c99ffbe9
Vinu Varghese <vinu@xminds.com>
2012-07-23 12:50:19 +0530

    [FIXES - #32961403] SquareStays.com sitemap pointing to the wrong URL

--- app/controllers/cached_sitemaps_controller.rb
+++ app/controllers/cached_sitemaps_controller.rb
@@ -1,4 +1,11 @@
 class CachedSitemapsController < SitemapsController
+
+  # Get the siteurl from the config - rather than guessing it
+  def default_url_options
+    {:host => SiteConfig.site_url.gsub('http://', ''),
+     :port => false}
+  end
+
   layout false
 
   before_filter :set_headers
@@ -9,9 +16,9 @@ class CachedSitemapsController < SitemapsController
   protected
   # When the page is cached, rails processes the action as html and sends the headers accordingly
   def set_headers
-   headers["Content-Type"] = "application/xml; charset=utf-8"
-   if not ActionController::Base.perform_caching  or not Rails.cache.exist? ActiveSupport::Cache.expand_cache_key "sitemap-xml", :views
-     headers["Last-Modified"] = DateTime.now.httpdate
-   end
+    headers["Content-Type"] = "application/xml; charset=utf-8"
+    if not ActionController::Base.perform_caching  or not Rails.cache.exist? ActiveSupport::Cache.expand_cache_key "sitemap-xml", :views
+      headers["Last-Modified"] = DateTime.now.httpdate
+    end
   end
 end

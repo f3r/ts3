bdeeb981f3a5515a1c0c61816faa34c5dc3dc2a7
baiju-xminds <baiju@xminds.in>
2012-08-02 15:06:25 +0530

    Adding DB schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -160,7 +160,7 @@ ActiveRecord::Schema.define(:version => 20120727034901) do
 
   create_table "cmspages", :force => true do |t|
     t.string  "page_title"
-    t.string  "page_url",         :default => "",     :null => false
+    t.string  "page_url",                             :null => false
     t.text    "description"
     t.boolean "active",           :default => false
     t.boolean "mandatory",        :default => false
@@ -210,7 +210,6 @@ ActiveRecord::Schema.define(:version => 20120727034901) do
     t.boolean "active",                :default => false
     t.integer "position"
     t.string  "currency_abbreviation"
-    t.boolean "default",               :default => false
   end
 
   create_table "delayed_jobs", :force => true do |t|
@@ -638,16 +637,14 @@ ActiveRecord::Schema.define(:version => 20120727034901) do
     t.text     "head_tag"
     t.text     "after_body_tag_start"
     t.text     "before_body_tag_end"
+    t.string   "color_scheme",                 :default => "default"
     t.string   "logo_file_name"
-    t.string   "fav_icon_file_name"
     t.string   "photo_watermark_file_name"
     t.string   "photo_watermark_content_type"
     t.integer  "photo_watermark_file_size"
     t.datetime "photo_watermark_updated_at"
+    t.string   "fav_icon_file_name"
     t.text     "sidebar_widget"
-    t.boolean  "charge_total",                 :default => false
-    t.integer  "fee_amount",                   :default => 300
-    t.boolean  "fee_is_fixed",                 :default => true
     t.boolean  "calendar",                     :default => true
     t.boolean  "enable_price_per_hour"
     t.boolean  "enable_price_per_day"

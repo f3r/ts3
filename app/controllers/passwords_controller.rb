235c24f9c89d0c606d7f77d168eb8d00a3f65fb7
Nicolas Gaivironsky <ngaivi@gmail.com>
2012-08-16 18:39:54 +0800

    Nicer invitation flow

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,4 +1,9 @@
 class PasswordsController < Devise::PasswordsController
-  layout 'single'
+  layout 'application'
 
+  def edit
+    self.resource = resource_class.new
+    resource.reset_password_token = params[:reset_password_token]
+    @user = User.find_by_reset_password_token(params[:reset_password_token])
+  end
 end

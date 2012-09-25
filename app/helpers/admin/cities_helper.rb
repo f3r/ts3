068ea01695e981359c7e10b45f31cefa5fac8c67
Nicolas Gaivironsky <ngaivi@gmail.com>
2012-09-25 17:33:15 +0800

    Maps on city search results

--- app/helpers/admin/cities_helper.rb
+++ app/helpers/admin/cities_helper.rb
@@ -9,9 +9,11 @@ module Admin::CitiesHelper
     html << link_to_if(city.active, 'View Public', public_city_path(city), :class => 'member_link', :target => '_blank')
   end
 
-  def static_image_map(product)
-    if product.geocoded?
-      image_tag "http://maps.googleapis.com/maps/api/staticmap?size=400x200&center=#{product.lat},#{product.lon}&zoom=14&sensor=false"
-    end
+  def static_image_map(product, zoom=14, pin = false)
+    return unless product.geocoded?
+    latlon = "#{product.lat},#{product.lon}"
+    url = "http://maps.googleapis.com/maps/api/staticmap?size=400x200&center=#{latlon}&zoom=#{zoom}&sensor=false"
+    url << "&markers=size:mid|color:red|#{latlon}" if pin
+    image_tag url
   end
 end

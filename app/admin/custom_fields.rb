a27421f4e8056a663fc38befd7c74fff36039860
Fer Martin <fer@heypal.com>
2012-09-30 18:36:55 +0800

    small comment fix

--- app/admin/custom_fields.rb
+++ app/admin/custom_fields.rb
@@ -30,7 +30,7 @@ ActiveAdmin.register CustomField  do
       f.input :required
       f.input :label
       f.input :hint
-      f.input :validations, :hint => "Validations reference: <a href = 'http://posabsolute.github.com/jQuery-Validation-Engine/#validators', target = '_blank'> link </a>".html_safe
+      f.input :validations, :hint => "Don't include validate[]! For validations reference: <a href = 'http://posabsolute.github.com/jQuery-Validation-Engine/#validators', target = '_blank'> link </a>".html_safe
       f.input :date_format, :as => :select, :collection => CustomField.DATE_FORMATS, :hint => "Only valid with date fields", :include_blank => false
       f.input :values
       f.input :more_info_label, :hint => "Only valid with Yes_No_Text field"
